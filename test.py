fdcef3df97ed91fcc8403c6190388006def25705
xDevansh <devjollani@Gmail.com>
2025-10-26 03:20:09 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+person={
+    "first_name":'Ravi',
+    "last_name":"Srinivas",
+    "age":20
+}
+
+print(f"The person's name is : {person['first_name']}")
+person["branch"]="Data Science"
+print(f"The person's branch is: {person['branch']}")
+
+print(person)
